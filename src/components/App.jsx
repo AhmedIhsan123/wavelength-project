69f29e2ca0e17613a2fc90e5d36c1aa4c7a23cd4
Ahmed Ihsan <Ihsan.Ahmed@student.greenriver.edu>
2026-04-23 10:29:26 -0700

    Setting up the artist component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import data from "../data.js";
+import Artist from "./Artist.jsx";
+import "../styles/App.css";
 
 function App() {
-	console.log(data);
+	// Get the artists data and map it to an array
+	const artistsData = data.artists.map((artist) => artist);
+	console.log(artistsData);
 	return (
 		<>
-			<h1>Hello World</h1>
+			<main>
+				<section className="history-container"></section>
+				<section className="artists-container">
+					{artistsData.map((artist) => (
+						<Artist key={artist.id} {...artist} />
+					))}
+				</section>
+			</main>
 		</>
 	);
 }
---
+++ src/components/Artist.jsx
@@ -0,0 +1,7 @@
+export default function Artist(props) {
+	return (
+		<>
+			<h1>{props.name}</h1>
+		</>
+	);
+}
